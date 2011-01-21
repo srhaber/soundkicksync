fb6d0720dcc0d73441d9d8e9e34f63a40ee51527
Shaun Haber <srhaber@gmail.com>
2011-01-21 11:59:40 -0800

    Update README, config-dist

--- README.md
+++ README.md
@@ -9,3 +9,20 @@ This app writes the upcoming tour dates from a Songkick artist to your Soundclou
 ## See the demo app!
 
 Test the app at [http://soundkicksync.srhaber.com](http://soundkicksync.srhaber.com).
+
+### SETUP
+
+To setup this app locally, you'll first need to [register a new application](http://soundcloud.com/you/apps/new) with Soundcloud.  Give the app an appropriate title.
+
+Additionally, you'll need to [apply for an api key](http://www.songkick.com/api_key_requests/new) with Songkick.
+
+1. Git clone this project to a web server and run the following commands to download the git submodules:
+
+	`git submodule init
+	git submodule update`
+
+1. Copy the config-dist.php to config.php.
+
+1. In config.php, enter the Soundcloud client key and secret and the Songkick API key.  Also enter the base URL for the app.
+
+The app should be up and running now.
--- config-dist.php
+++ config-dist.php
@@ -1,18 +1,39 @@
 <?php
+/**
+ * The base url is the main hostname for this app.
+ * Leave out the trailing slash.
+ * E.g. $base_url = 'http://example.com'
+ */
 $base_url = '';
-$redirect_uri = '';
 
+/**
+ * The redirect URI is used for the oauth2 callback.
+ */
+$redirect_uri = $base_url . '/oauthcallback.php';
+
+/**
+ * Soundcloud client credentials.  These are available on app page.
+ * http://soundcloud.com/you/apps/[title]/edit
+ */
 $client_id = '';
 $client_secret = '';
 
+/**
+ * Songkick API key.  You may need to apply for one.
+ * http://www.songkick.com/api_key_requests/new
+ */
 $songkick_api_key = '';
 
+/**
+ * These CURL options are added to all CURL requests.  Add or
+ * modify these as needed by the app.
+ */
 $curl_options = array(
   CURLOPT_SSL_VERIFYPEER => false, 
   CURLOPT_SSL_VERIFYHOST => false,
 );
 
-// ** DON'T EDIT BELOW THIS LINE **
+/*** DON'T EDIT BELOW THIS LINE ***/
 
 include 'modules/soundcloud/Services/Soundcloud.php';
 include 'lib.php';
